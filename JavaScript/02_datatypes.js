269c0c8a519d41cb97dd0ce4816aaf5a78c25b72
Satyam Kumar <141249496+satyam-singh24@users.noreply.github.com>
2024-02-04 21:25:50 +0000

    datatypes

---
+++ JavaScript/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log("Satyam")
+
+let name = "Satyam"
+let age = 23
+let isLoggedIn = false
+
+// Data Types :-
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null)
+console.log(typeof age);
