a97f25d867bb58569852b79cf873752f1979bfdf
Satyam Kumar <141249496+satyam-singh24@users.noreply.github.com>
2024-02-06 20:00:02 +0000

    strings

---
+++ JavaScript/05_strings.js
@@ -0,0 +1,34 @@
+const name = "satyam"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('satyam-sk-hack')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Satyam   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://eminent.com/eminent%20singh"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('ethical'));
+
+console.log(gameName.split('-'));
