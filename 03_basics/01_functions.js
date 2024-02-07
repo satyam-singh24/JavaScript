73a0b350d4e66160fb649238fa953aeb970f8661
Satyam Kumar <141249496+satyam-singh24@users.noreply.github.com>
2024-02-07 20:19:42 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,36 @@ function loginUserMessage(username = "satyam") {
 }
 
 // console.log(loginUserMessage("satyam"))
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "satyam",
+    prices: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "satyam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
