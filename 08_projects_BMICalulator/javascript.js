317a8275caff555150b8718aa7bbac04ebebfe95
Satyam Kumar <141249496+satyam-singh24@users.noreply.github.com>
2024-02-11 22:28:34 +0000

    project solution code

--- 08_projects_BMICalulator/javascript.js
+++ 08_projects_BMICalulator/javascript.js
@@ -4,11 +4,11 @@ const form = document.querySelector('form')
 // const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function(e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  const height = parseInt(document.querySelector('#height').value)
-  const weight = parseInt(document.querySelector('#weight').value)
-  const results = document.querySelector('#results')
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
 
   if (height = '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
@@ -17,10 +17,10 @@ form.addEventListener('submit', function(e) {
     results.innerHTML = `Please give a valid weight ${weight}`;
   }
   else{
-    const bmi = (weight / ((height * height)/10000)).toFixed(2)
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
     // show result
     results.innerHTML = `<span>${bmi}</span>`;
   }
 
-})
+});
